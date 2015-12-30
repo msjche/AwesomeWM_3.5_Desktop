f62f6977d451582e2a61a45b0e7b5216f01a6032
msjche <msjche@gmail.com>
2015-12-30 15:42:43 -0800

    remove launcher

--- PROFILES/msjche/rc.lua
+++ PROFILES/msjche/rc.lua
@@ -457,7 +457,7 @@ for s = 1, screen.count() do
     right_layout:add(uptimewidget)
     right_layout:add(space)
     right_layout:add(mytextclock)
-    right_layout:add(Default_launcher)
+--    right_layout:add(Default_launcher)
     right_layout:add(mylayoutbox[s])
 
     -- Now bring it all together (with the tasklist in the middle)
